d3814822992c0e82922d4083caf48c2b69b7071d
Goutamraj Sahu <64259409+GoutamrajSahu@users.noreply.github.com>
2021-05-26 01:15:11 +0530

    Add variables to html code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const num = "7";
+const fName = "Gotamraj";
+const lName = "Sahu";
+const currentYear = new Date().getFullYear();
+
 ReactDom.render(
   <div>
     <h1>My Favourite Foods</h1>
@@ -8,7 +13,15 @@ ReactDom.render(
       <li>Panir</li>
       <li>Sandwitch</li>
       <li>Pizza</li>
+      <li>Biriyani</li>
     </ul>
+    <h2>My lucky number is {num}</h2>
+    <h2>Hello {fName + " " + lName}</h2>
+    <h2>
+      Hello {fName} {lName}
+    </h2>
+    <p>Created by {fName + " " + lName}.</p>
+    <p>Copyright {currentYear}.</p>
   </div>,
   document.getElementById("root")
 );
