1f08ae810016d9dc5cca276712a8a2821a6a6bdb
Goutamraj Sahu <64259409+GoutamrajSahu@users.noreply.github.com>
2021-05-26 00:06:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Panir</li>
+      <li>Sandwitch</li>
+      <li>Pizza</li>
+      <li>Biriyani</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
